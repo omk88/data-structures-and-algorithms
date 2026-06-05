07d65380ba0a997d464a842b1421374e64a507de
Emmanuel Learmount <oldmanjenkins56@gmail.com>
2026-06-05 14:51:13 +0100

    Sub array target and has substring anagram

--- SlidingWindow/SlidingWindow.py
+++ SlidingWindow/SlidingWindow.py
@@ -49,8 +49,45 @@ def multiplication_max_subarray_sum_size_k(nums, k):
 
     return max_product
 
+# Check that this is correct!
+def subarray_target_sum_size_k(nums, target, k):
+    current_sum = 0
+    for i in range(0, k):
+        current_sum += nums[i]
+
+    count = 1 if current_sum == target else 0
+
+    for i in range(0, len(nums) - k):
+        current_sum -= nums[i]
+        current_sum += nums[i + k]
+
+        if current_sum == target:
+            count += 1
+    
+    return count
+
+
+def has_substring_anagram(s, anagram):
+    k = len(anagram)
+    window_set = set(s[:k])
+    anagram_set = set(anagram)
+    if window_set == anagram_set:
+        return True
+    
+    for i in range(0, len(s) - k):
+        window_set.remove(s[i])
+        window_set.add(s[i + k])
+        if window_set == anagram_set:
+            return True
+        
+    return False
+
 print(naive_max_subarray_sum_size_k([4, 2, 1, -9, 8, 4, 3], 3))
 
 print(optimal_max_subarray_sum_size_k([4, 2, 1, -9, 8, 4, 3], 3))
 
 print(multiplication_max_subarray_sum_size_k([1, 4, 1, 6, -3, 3, -5, 2, 26], 4))
+
+print(subarray_target_sum_size_k([1, 2, 1, 5, 2, 3, 10, 1, 9, 4, 3, 3, 7], 10, 3))
+
+print(has_substring_anagram("greyhounds", "hoy"))
